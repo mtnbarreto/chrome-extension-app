5ee64870c74e4382ac820c8deb8324eb237d40bc
Martin Barreto <mtn.barreto@gmail.com>
2023-06-05 00:49:32 -0300

    first commit

---
+++ src/components/Actions.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './../components/HelloWorld.vue'
+</script>
+
+<template>
+    <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style>
+</style>
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json' // Node 14 & 16
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'pages/index.html',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    }, 
+  },
+  plugins: [vue(),
+            crx({ manifest }), 
+           ],
+})
