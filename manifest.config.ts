37bd549d5bc918a1bb8d8980aa1a7762f7c91824
Martin Barreto <mtn.barreto@gmail.com>
2023-06-09 22:04:29 -0300

    rome format fixes

--- manifest.config.ts
+++ manifest.config.ts
@@ -21,7 +21,7 @@ export default defineManifest(async (env) => ({
     '128': 'icons/icon-128.png',
   },
   action: {
-    default_popup: "src/pages/increment/index.html",
+    default_popup: 'src/pages/increment/index.html',
     default_title: 'Default Action Title, default_popup: index.html',
   },
   background: {
@@ -30,7 +30,7 @@ export default defineManifest(async (env) => ({
   content_scripts: [
     {
       js: ['src/pages/content/actions.js'],
-      matches: ["https://mail.google.com/**/*"],
+      matches: ['https://mail.google.com/**/*'],
     },
   ],
   manifest_version: 3,
